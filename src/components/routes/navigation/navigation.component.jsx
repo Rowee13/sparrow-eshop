82630adfa6ece53de2236c20777467f2ef1a8f9b
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-11-03 18:47:28 +0800

    Setup navigation and react route
    Installed firebase as dependency

---
+++ src/components/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrownLogo } from "../../../assets/crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+	return (
+		<Fragment>
+			<div className="navigation">
+				<Link to="/" className="logo-container">
+					<CrownLogo className="logo" />
+				</Link>
+				<div className="nav-links-container">
+					<Link to="/shop" className="nav-link">
+						SHOP
+					</Link>
+					<Link to="/signin" className="nav-link">
+						SIGNIN
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</Fragment>
+	);
+};
+
+export default Navigation;
